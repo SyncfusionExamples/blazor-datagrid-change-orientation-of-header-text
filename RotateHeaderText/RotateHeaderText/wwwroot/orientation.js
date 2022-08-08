98369be07742cd4ffc780092df438b340a989ece
Vignesh Natarajan <vignesh.natarajan@syncfusion.com>
2022-08-08 11:20:31 +0530

    sample

---
+++ RotateHeaderText/RotateHeaderText/wwwroot/orientation.js
@@ -0,0 +1,7 @@
+﻿function setHeaderHeight(args) {
+    var textWidth = document.querySelector(".e-headercell > div").scrollWidth; // obtain the width of the headerText content.
+    var headerCell = document.querySelectorAll(".e-headercell");
+    for (var i = 0; i < headerCell.length; i++) {
+        (headerCell.item(i)).style.height = textWidth + 'px'; // assign the obtained textWidth as the height of the headerCell.
+    }
+}
